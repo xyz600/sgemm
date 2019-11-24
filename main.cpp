a8b4afbf04d0c8ed480a6dda4eb65c5f91513abf
xyz600 <xyz600600@gmail.com>
2019-11-24 18:28:28 +0900

    initialize value

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <chrono>
 #include <iostream>
+#include <random>
 
 #include "matrix_cpu.hpp"
 
@@ -8,6 +9,18 @@ std::size_t experiment_cpu(const std::size_t size, const std::size_t iteration)
     sgemm_cpu::MatrixCPU matrix1(size);
     sgemm_cpu::MatrixCPU matrix2(size);
     sgemm_cpu::MatrixCPU result(size);
+
+    std::mt19937_64 mt(0);
+    std::uniform_real_distribution<float> rand;
+
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            matrix1.value(i, j) = rand(mt);
+            matrix2.value(i, j) = rand(mt);
+        }
+    }
 
     std::size_t elapsed = 0;
 
@@ -27,7 +40,7 @@ int main(int argc, char* argv[])
     constexpr std::size_t size = 512;
     constexpr std::size_t iteration = 1;
 
-    auto average_elapsed = experiment_cpu(size, iteration);
+    const auto average_elapsed = experiment_cpu(size, iteration);
 
     std::cout << "average time: " << average_elapsed << "[us]" << std::endl;
 
