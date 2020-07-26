544f40adaf6d26a27b86f9303b036932e6978af5
xyz600 <xyz600600@gmail.com>
2020-07-26 15:39:56 +0900

    cache blocking

--- matrix/matrix_cpu.cpp
+++ matrix/matrix_cpu.cpp
@@ -37,14 +37,25 @@ void MatrixCPU::multiply_fast(const MatrixCPU& right, MatrixCPU& out) const noex
     assert(right.size_ == size_);
     assert(size_ == out.size_);
 
+    constexpr std::size_t block_size = 64;
+
 #pragma omp parallel for
-    for (std::size_t i = 0; i < size_; i++)
+    for (std::size_t i = 0; i < size_; i += block_size)
     {
-        for (std::size_t k = 0; k < size_; k++)
+        for (std::size_t k = 0; k < size_; k += block_size)
         {
-            for (std::size_t j = 0; j < size_; j++)
+            for (std::size_t j = 0; j < size_; j += block_size)
             {
-                out[i * stride_ + j] += data_[i * stride_ + k] * data_[k * stride_ + j];
+                for (std::size_t ii = i; ii < i + block_size; ii++)
+                {
+                    for (std::size_t kk = k; kk < k + block_size; kk++)
+                    {
+                        for (std::size_t jj = j; jj < j + block_size; jj++)
+                        {
+                            out[ii * stride_ + jj] += data_[ii * stride_ + kk] * right.data_[kk * stride_ + jj];
+                        }
+                    }
+                }
             }
         }
     }
